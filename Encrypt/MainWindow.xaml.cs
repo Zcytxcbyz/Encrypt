a51f35aed04c847fae0040f4463f7911970c3e94
zcytxcbyz@163.com <zcytxcbyz@163.com>
2019-10-01 19:15:39 +0800

    First Commit

--- Encrypt/MainWindow.xaml.cs
+++ Encrypt/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Encrypt
 {
@@ -20,9 +13,101 @@ namespace Encrypt
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string SettingFilePath =
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\"
+            + Path.GetFileNameWithoutExtension(System.Windows.Forms.Application.ExecutablePath)
+            + ".dat";
+
         public MainWindow()
         {
-            InitializeComponent();
+                InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TitleLabel.Content = Properties.Resources.TitleLabelText;
+            KeyLabel.Content = Properties.Resources.KeyLabelText;
+            EncryptButton.Content = Properties.Resources.EncryptButtonText;
+            DecryptButton.Content = Properties.Resources.DecryptButtonText;
+            CloseButton.ToolTip = Properties.Resources.CloseButtonToolTip;
+            MinButton.ToolTip = Properties.Resources.MinButtonToolTip;
+            if (File.Exists(SettingFilePath))
+            {
+                FileStream fileStream = new FileStream(SettingFilePath, FileMode.OpenOrCreate, FileAccess.Read);
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                this.Top = binaryReader.ReadDouble();
+                this.Left = binaryReader.ReadDouble();
+                binaryReader.Close();
+                fileStream.Close();
+            }
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            string str = GetText(MainTextBox);
+            string key = KeyTextBox.Text;
+            string result = AESCrypto.AES.AESEncrypt(str, key);
+            if (result != null)
+            {
+                LoadText(MainTextBox, result, Brushes.Black);
+            }
+            else
+            {
+                LoadText(MainTextBox, AESCrypto.AES.ErrInfo, Brushes.Red);
+            }
+        }
+
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            string str = GetText(MainTextBox);
+            string key = KeyTextBox.Text;
+            string result = AESCrypto.AES.AESDecrypt(str, key);
+            if (result != null)
+            {
+                LoadText(MainTextBox, result, Brushes.Black);
+            }
+            else
+            {
+                LoadText(MainTextBox, AESCrypto.AES.ErrInfo, Brushes.Red);
+            }
+        }
+        private string GetText(RichTextBox richTextBox)
+        {
+            TextRange textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+            return textRange.Text;
+        }
+        private void LoadText(RichTextBox richTextBox, string txtContent, Brush Color)
+        {
+            richTextBox.Document.Blocks.Clear();
+            richTextBox.Document.Blocks.Add(new Paragraph(new Run(txtContent) {Foreground=Color }));
+            richTextBox.Document.Blocks.Add(new Paragraph(new Run() { Foreground = Brushes.Black }));
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            FileStream fileStream = new FileStream(SettingFilePath, FileMode.Create, FileAccess.Write);
+            BinaryWriter binaryWriter = new BinaryWriter(fileStream);
+            binaryWriter.Write(this.Top);
+            binaryWriter.Write(this.Left);
+            binaryWriter.Close();
+            fileStream.Close();
+            Environment.Exit(0);
         }
     }
 }
+
